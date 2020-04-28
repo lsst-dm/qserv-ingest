8cf73ff836e04972dbf2c721f17b86c525832698
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-04-28 13:25:44 +0200

    Create ingest tmp directory

--- load.sh
+++ load.sh
@@ -2,8 +2,9 @@
 
 set -euxo pipefail
 
-WORKER="qserv-worker-1"
+WORKER="qserv-worker-0"
 PORT="25002"
 kubectl exec -it "$WORKER" -c repl-wrk -- sh -c "cd /tmp && \
   curl -lO https://raw.githubusercontent.com/lsst-dm/qserv-DC2/tickets/DM-24587/data/step1_1/chunk_57892.txt && \
+  mkdir -p /qserv/data/ingest && \
   qserv-replica-file-ingest FILE localhost $PORT 1 position P /tmp/chunk_57892.txt"
