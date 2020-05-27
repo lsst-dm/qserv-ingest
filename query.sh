1f2d6f27f15d16de519b9e04a7aa6d8fcf65c0ef
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-05-27 22:27:12 +0200

    upd

--- query.sh
+++ query.sh
@@ -6,8 +6,10 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 INSTANCE="qserv"
 
-CZAR_POD="${INSTANCE}-czar-0"
+CZAR_POD="${INSTANCE}-czar-0.${INSTANCE}-czar"
 
 # FIXME restart qserv to refresh xrootd cache
 kubectl delete pod -l app=$INSTANCE
-kubectl exec -it "$CZAR_POD" -- bash -lc ". /qserv/stack/loadLSST.bash && setup mariadb -t qserv-dev && mysql --host 127.0.0.1 --port 4040 --user qsmaster -e 'select * from desc_dc2.position'"
+kubectl exec -it qserv-ingest -- bash -lc ". /stack/loadLSST.bash && \
+    setup mariadb -t qserv-dev && \
+    mysql --host $CZAR_HOST --port 4040 --user qsmaster -e 'select * from desc_dc2.position'" 
