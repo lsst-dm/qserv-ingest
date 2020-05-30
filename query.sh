006f0954599cb27e471593e58daabca2b4e865c2
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-05-30 19:35:45 +0200

    upd

--- query.sh
+++ query.sh
@@ -7,4 +7,5 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 CZAR_HOST="${INSTANCE}-czar-0.${INSTANCE}-czar"
 
-kubectl exec -it $INSTANCE-ingest-db-0 -- bash -lc "mysql --host $CZAR_HOST --port 4040 --user qsmaster -e 'select * from desc_dc2.position'" 
+kubectl exec -it $INSTANCE-ingest-db-0 -- \
+    bash -lc "mysql --host $CZAR_HOST --port 4040 --user qsmaster -e 'SELECT * FROM desc_dc2.position LIMIT 10'"
