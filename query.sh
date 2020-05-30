cda9f6741a2c24efccda444e45fa4675560e359a
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-05-30 12:53:01 +0200

    upd

--- query.sh
+++ query.sh
@@ -3,12 +3,8 @@
 set -euxo pipefail
 
 DIR=$(cd "$(dirname "$0")"; pwd -P)
-
-INSTANCE="qserv"
+. "$DIR"/env.sh
 
 CZAR_HOST="${INSTANCE}-czar-0.${INSTANCE}-czar"
 
-# FIXME restart qserv to refresh xrootd cache
-kubectl delete pod -l app=$INSTANCE
-kubectl wait pod --for=condition=Ready -l "app=qserv"
 kubectl exec -it $INSTANCE-ingest-db-0 -- bash -lc "mysql --host $CZAR_HOST --port 4040 --user qsmaster -e 'select * from desc_dc2.position'" 
