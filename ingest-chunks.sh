e3d7e8c7b8084ed05d3c40bd3038de08c6cdb0e0
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-06-03 08:42:16 +0200

    upd

--- ingest-chunks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-DIR=$(cd "$(dirname "$0")"; pwd -P)
-
-JOB="ingest-chunks"
-kubectl apply -f "$DIR/manifest/$JOB.yaml"
-kubectl wait --for=condition=complete "job/$JOB"
--- job.sh
+++ job.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 
-JOB="ingest-register"
+JOB="ingest-$1"
 kubectl apply -f "$DIR/manifest/$JOB.yaml"
-kubectl wait --for=condition=complete "job/$JOB"
+kubectl wait --for=condition=complete --timeout=-1s "job/$JOB"
