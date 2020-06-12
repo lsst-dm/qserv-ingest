49dd0ea6d979e93814315e0dca64e638a8266a2a
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-06-12 23:52:43 +0200

    upd

--- query.sh
+++ query.sh
@@ -8,7 +8,7 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 CZAR_HOST="${INSTANCE}-czar-0.${INSTANCE}-czar"
 
 kubectl exec -it $INSTANCE-ingest-db-0 -- \
+    bash -lc "mysql --host $CZAR_HOST --port 4040 --user qsmaster -e 'SELECT * FROM dc2_run2_1i_dr1b.position LIMIT 10'"
+kubectl exec -it $INSTANCE-ingest-db-0 -- \
     bash -lc "mysql --host $CZAR_HOST --port 4040 --user qsmaster -e 'SELECT COUNT(*) FROM dc2_run2_1i_dr1b.position'"
 
-kubectl exec -it $INSTANCE-ingest-db-0 -- \
-    bash -lc "mysql --host $CZAR_HOST --port 4040 --user qsmaster -e 'SELECT * FROM dc2_run2_1i_dr1b.position LIMIT 10'"
