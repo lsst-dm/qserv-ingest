ecff99fe9c2d424ae12ce48c81fffd876005331f
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-06-17 21:54:07 +0200

    upd

--- create-ingest.sh
+++ create-ingest.sh
@@ -6,7 +6,7 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 . ./env.sh
 
 kubectl apply -k $DIR/base/dev
-while ! kubectl wait pod --for=condition=Ready --timeout="10s" -l "app=qserv,tier=ingest,instance=$INSTANCE"
+while ! kubectl wait pod --for=condition=Ready --timeout="10s" -l "app=qserv,tier=ingest-dev"
 do
   echo "Wait for Qserv ingest pods to be ready:"
   kubectl get pod -l "app=qserv,tier=ingest-dev"
