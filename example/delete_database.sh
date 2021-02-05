89b89b4ad2b4df9a450eec9311716376ec731c27
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-02-05 17:22:11 +0100

    Fix metadata for 2ndary index generation
    - Add example script to create secondary index
    - Update documentation
    - Generate doc using github actions

---
+++ example/delete_database.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Get the number of remaining elements in ingest chunk queue
+
+set -euxo pipefail
+
+DIR=$(cd "$(dirname "$0")"; pwd -P)
+. "$DIR"/../env.sh
+
+DATABASE="cosmoDC2_v1_1_4_image_overlap"
+PASSWORD=CHANGEME
+
+kubectl exec -it $INSTANCE-ingest-db-0 -- mysql -h localhost -u root -p"$PASSWORD" -e "DELETE FROM qservIngest.task WHERE \`database\` LIKE '$DATABASE';"
+time kubectl exec -it $INSTANCE-repl-ctl-0 -- curl http://localhost:25080/ingest/database/"$DATABASE"  \
+   -X DELETE -H "Content-Type: application/json" \
+   -d '{"auth_key":""}'
+
