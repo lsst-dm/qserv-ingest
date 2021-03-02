690233b1caf5c5e5b3a307a99036031c7d94b4bb
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-03-02 21:39:03 +0100

    Revert replication controller port to 25080

--- rootfs/ingest/bin/env.sh
+++ rootfs/ingest/bin/env.sh
@@ -6,5 +6,5 @@ data_url=$(echo $data_url | sed 's%\(.*[^/]\)/*%\1%')
 
 QUEUE_URL="mysql://qsingest:@qserv-ingest-db-0.qserv-ingest-db/qservIngest"
 QSERV_INGEST_DIR="/ingest/data"
-REPL_URL="http://qserv-repl-ctl-0.qserv-repl-ctl:8080"
+REPL_URL="http://qserv-repl-ctl-0.qserv-repl-ctl:25080"
 
