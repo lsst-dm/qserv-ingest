03180fa4d4e913d97fac6fc41fd60b007764f8c3
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-04-12 08:02:44 +0200

    Improve argo workflow install script

--- argo-install.sh
+++ argo-install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Install Argo workflow pods and client
+
 set -euxo pipefail
 
+# Install argo workflow
 kubectl apply -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/quick-start-postgres.yaml
 kubectl patch configmaps/workflow-controller-configmap -p '{"data":{"containerRuntimeExecutor":"k8sapi"}}'
 
@@ -9,3 +12,16 @@ NS=$(kubectl config view --minify --output 'jsonpath={..namespace}')
 NS=$([ ! -z "$NS" ] && echo "$NS" || echo "default")
 
 kubectl create rolebinding --clusterrole=edit --serviceaccount="$NS":default argo
+
+# Install argo client
+ARGO_CLIENT_VERSION="v2.12.11"
+# Download the binary
+curl -sLO https://github.com/argoproj/argo/releases/download/v2.12.11/argo-linux-amd64.gz
+# Unzip
+gunzip argo-linux-amd64.gz
+# Make binary executable
+chmod +x argo-linux-amd64
+# Move binary to path
+sudo mv ./argo-linux-amd64 /usr/local/bin/argo
+
+kubectl wait --for=condition=available --timeout=600s deployment argo-server minio postgres workflow-controller
--- env.example.sh
+++ env.example.sh
@@ -1,5 +1,5 @@
-GIT_HASH="$(git describe --dirty --always)"
-TAG=${OP_VERSION:-${GIT_HASH}}
+GIT_HASH="$(git -C $DIR describe --dirty --always)"
+TAG=${INGEST_VERSION:-${GIT_HASH}}
 
 BASE_IMAGE="python:3.9.0-alpine3.12"
 # Image version created by build procedure
@@ -7,5 +7,7 @@ INGEST_IMAGE="qserv/ingest:$TAG"
 INGEST_DEPS_IMAGE="qserv/ingest-deps:latest"
 INSTANCE="qserv"
 OVERLAY="base"
+#OVERLAY="in2p3-dc2_dr6_object_v2"
+#OVERLAY="dc2-errors"
 
 REPL_CTL_PORT="8080"
