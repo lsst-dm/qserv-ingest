85f63210c6733720248c77dc5d0e718530fb8c15
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-05-06 12:29:56 +0200

    upd

--- example/list_transactions.TODO.sh
+++ example/list_transactions.TODO.sh
@@ -7,7 +7,8 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 . "$DIR"/../env.sh
 
 # DATABASE="dc2_object_run2_2i_dr6_wfd"
-DATABASE="cosmoDC2_v1_1_4_image_overlap"
+#DATABASE="cosmoDC2_v1_1_4_image_overlap"
+DATABASE="cosmoDC2_v1_1_4_image"
 
 PASSWORD=""
 kubectl exec -it qserv-repl-ctl-0 -- curl "http://qserv-repl-ctl:$REPL_CTL_PORT/ingest/trans/?database=$DATABASE" -X GET  -H "Content-Type: application/json" -d "{\"auth_key\":\"$PASSWORD\"}"
