f1cb2e05a1c31f7f965dcc205135fa77985a0930
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-12-10 19:38:48 +0100

    Improve Qserv install script in CI

---
+++ tests/e2e/install-qserv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install qserv-operator and Qserv
+
+# @author Fabrice Jammes SLAC/IN2P3
+
+set -euxo pipefail
+
+OPERATOR_VERSION="2021.10.1-rc1-28-g0f9fd21"
+OPERATOR_DIR="/tmp/qserv-operator"
+if [ -d "$OPERATOR_DIR" ]; then
+  rm -rf "$OPERATOR_DIR"
+fi
+
+git clone https://github.com/lsst/qserv-operator "$OPERATOR_DIR"
+git -C "$OPERATOR_DIR" checkout "$OPERATOR_VERSION"
+kubectl apply -f "$OPERATOR_DIR"/manifests/operator.yaml
+kubectl apply -k "$OPERATOR_DIR"/manifests/base
+"$OPERATOR_DIR"/tests/tools/wait-qserv-ready.sh
