87195a9254b9d7dae4d118e29f73c76dbb7b8986
Fritz Mueller <fritzm@slac.stanford.edu>
2023-07-14 21:42:49 -0700

    Publish new release 2023.7.1-rc3

--- env.example.sh
+++ env.example.sh
@@ -1,6 +1,6 @@
 # Set variable below to empty string in order to install
 # current development version
-INGEST_RELEASE='2023.7.1-rc2'
+INGEST_RELEASE='2023.7.1-rc3'
 TAG=${INGEST_RELEASE:-$(git -C $DIR describe --dirty --always)}
 
 # Select dataset to load
